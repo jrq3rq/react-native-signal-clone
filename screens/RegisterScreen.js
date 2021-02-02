96ecfd641e5b55091d108a9cc87691cc9d4f121e
YOUR NAME <YOUR EMAIL ADDRESS>
2021-02-01 18:27:55 -0800

    setting up auth

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,16 +1,87 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useLayoutEffect } from "react";
+import { StyleSheet, View, KeyboardAvoidingView } from "react-native";
+import { Button, Input, Text } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
 
 const RegisterScreen = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackTitle: "Back to Login",
+    });
+  }, [navigation]);
+
+  const register = () => {};
+
   return (
-    <View>
-      <Text>this is the register screen</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Text h3 style={{ marginBottom: 50 }}>
+        Create a Signal account
+      </Text>
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Full Name"
+          autoFocus
+          type="text"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <Input
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          type="text"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Input
+          placeholder="Profile Picture URL (optional)"
+          type="text"
+          value={imgUrl}
+          onChangeText={(text) => setImgUrl(text)}
+          onSubmitEditing={register}
+          //   register
+        />
+      </View>
+      <Button
+        containerStyle={styles.button}
+        raised
+        onPress={register}
+        title="Register"
+      />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
   );
 };
 
 export default RegisterScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    paddingTop: 40,
+    backgroundColor: "white",
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+  inputContainer: {
+    width: 300,
+  },
+});
 
 // rnfes
