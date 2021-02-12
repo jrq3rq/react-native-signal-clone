09dec4fc44c995bf0497a505e5694fab25fca436
YOUR NAME <YOUR EMAIL ADDRESS>
2021-02-11 17:10:24 -0800

    finalized chat ui

--- screens/components/CustomListItem.js
+++ screens/components/CustomListItem.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { ListItem, Avatar } from "react-native-elements";
+import { useEffect } from "react";
+import { db } from "../../firebase";
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+  const [chatMessage, setChatMessages] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection("adds")
+      .doc(id)
+      .collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setChatMessages(snapshot.docs.map((doc) => doc.data()))
+      );
+    return unsubscribe;
+  });
+
   return (
-    <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
+    <ListItem
+      key={id}
+      onPress={() => enterChat(id, chatName)}
+      key={id}
+      bottomDivider
+    >
       <Avatar
         rounded
         source={{
           uri:
+            chatMessage?.[0]?.photoURL ||
             "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
         }}
       />
@@ -17,7 +39,7 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
           {chatName}
         </ListItem.Title>
         <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-          ABC
+          {chatMessage?.[0]?.displayName}: {chatMessage?.[0]?.message}
         </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
